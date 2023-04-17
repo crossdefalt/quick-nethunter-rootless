446e263c1bbc87168cdd90bb23bfb1d90e06b8a6
Defalt <106070707+crossdefalt@users.noreply.github.com>
2023-04-18 04:28:30 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,5 @@
-(echo "1" && cat) | pkg install wget -y && wget -O install-nethunter-termux https://offs.ec/2MceZWr && chmod +x install-nethunter-termux && ./install-nethunter-termux
+#!/bin/bash
+pkg install wget -y
+wget -O install-nethunter-termux https://offs.ec/2MceZWr
+chmod +x install-nethunter-termux
+echo "1" | ./install-nethunter-termux
