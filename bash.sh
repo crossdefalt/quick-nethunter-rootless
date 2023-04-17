ee185f4d1e85eb5e22ee1b09c19898075de70b67
Defalt <106070707+crossdefalt@users.noreply.github.com>
2023-04-18 04:45:10 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+# Install Nethunter
 pkg install wget -y
 wget -O install-nethunter-termux https://offs.ec/2MceZWr
 chmod +x install-nethunter-termux
-echo "1" | ./install-nethunter-termux
+./install-nethunter-termux
+
+# Delete files
+rm -f install-nethunter-termux
+rm -f kalifs-arm64-full.sha512sum
+rm -f kalifs-arm64-full.tar.xz
