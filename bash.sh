cf5380e23be75608fc7691391fe0527deb4bbc5b
Defalt <106070707+crossdefalt@users.noreply.github.com>
2023-04-18 06:22:20 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -7,37 +7,18 @@ apt upgrade -y -o Dpkg::Options::="--force-confold"
 pkg install wget -y
 wget -O install-nethunter-termux https://offs.ec/2MceZWr
 chmod +x install-nethunter-termux
-
-# Loop to continue if [?] is encountered
-while grep -q '\[?\]' install-nethunter-termux; do
+./install-nethunter-termux | while read line; do
+  if [[ "$line" == *"[?]"* && "$line" != *"[?][?]"* ]]; then
     read -p "The script has encountered a [?] character. Please type 'y' and press enter to continue, or press any other key to exit: " choice
     if [ "$choice" == "y" ]; then
-        sed -i 's/\[?\]/y/g' install-nethunter-termux
+      echo "y" >> /tmp/install-nethunter-termux-answers.txt
     else
-        echo "Installation aborted."
-        exit 1
+      echo "Installation aborted."
+      exit 1
     fi
+  else
+    echo "$line"
+  fi
 done
 
-# Run the installer
-echo "Installing Nethunter. This may take a while..."
-./install-nethunter-termux
-
-# Check for errors
-if [ $? -ne 0 ]; then
-    echo "Nethunter installation failed."
-    exit 1
-fi
-
-# Clean up
-rm install-nethunter-termux
-
-echo "Installation finished. Type 'nh' to start Nethunter."
-read -n 2 -p "> " start_nethunter
-if [ "$start_nethunter" == "nh" ]; then
-    # Start Nethunter
-    # ...
-    echo "Nethunter started."
-else
-    echo "Nethunter not started."
-fi
+echo "Installation finished. Use 'nh' command to start Nethunter."
