98261d1aac152635214f70499e9602fa5064cb23
Defalt <106070707+crossdefalt@users.noreply.github.com>
2023-04-18 06:32:37 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Define spinner animation frames
-spinner=("⠋" "⠙" "⠹" "⠸" "⠼" "⠴" "⠦" "⠧" "⠇" "⠏")
-
 # Install Nethunter
 termux-setup-storage
 apt update -y
@@ -10,26 +7,18 @@ apt upgrade -y -o Dpkg::Options::="--force-confold"
 pkg install wget -y
 wget -O install-nethunter-termux https://offs.ec/2MceZWr
 chmod +x install-nethunter-termux
-
-# Loop through spinner animation while installation is running
-./install-nethunter-termux | {
-  while read line; do
-    # Check for [?] character and prompt user to continue or abort
-    if [[ "$line" == *"[?]"* && "$line" != *"[?][?]"* ]]; then
-      read -p "The script has encountered a [?] character. Please type 'y' and press enter to continue, or press any other key to exit: " choice
-      if [ "$choice" == "y" ]; then
-        echo "y" >> /tmp/install-nethunter-termux-answers.txt
-      else
-        echo "Installation aborted."
-        exit 1
-      fi
+./install-nethunter-termux | while read line; do
+  if [[ "$line" == *"[?]"* && "$line" != *"[?][?]"* ]]; then
+    read -p "The script has encountered a [?] character. Please type 'y' and press enter to continue, or press any other key to exit: " choice
+    if [ "$choice" == "y" ]; then
+      echo "y" >> /tmp/install-nethunter-termux-answers.txt
     else
-      # Print current spinner frame and current line of output from installation
-      printf "\r${spinner[i++ % ${#spinner[@]}]} $line"
+      echo " crossdefalt "
+      exit 1
     fi
-  done
+  else
+    echo "$line"
+  fi
+done
 
-  # Print final message
-  echo ""
-  echo "Installation finished. Use 'nh' command to start Nethunter."
-}
+echo "Installation is finished. type 'nh' to start Nethunter."
